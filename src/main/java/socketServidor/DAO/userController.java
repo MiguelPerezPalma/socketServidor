e2d2c080ad69dc524e2662016fbe11f57195062a
Alvaro L <alvarooleto@gmail.com>
2022-02-28 18:54:32 +0100

    primera subida userController

--- src/main/java/socketServidor/DAO/userController.java
+++ src/main/java/socketServidor/DAO/userController.java
@@ -1,5 +1,57 @@
 package socketServidor.DAO;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
+import socketServidor.models.user;
+import socketServidor.utils.PersistenceUnit;
+
 public class userController {
 
+	public static EntityManager createEM() {
+		return PersistenceUnit.getEM();
+	}
+
+	public List<user> showAllUsers() throws Exception {
+		List<user> result = new ArrayList<user>();
+		EntityManager em = createEM();
+
+		try {
+			em.getTransaction().begin();
+			TypedQuery<user> q = em.createNamedQuery("getAllUsers", user.class);
+			result = q.getResultList();
+			em.getTransaction().commit();
+
+		} catch (Exception e) {
+			throw new Exception("Hubo algún error", e);
+		}
+
+		return result;
+	}
+	
+	
+	public user showUser(int id) throws Exception {
+		user result = null;
+		
+		EntityManager em = createEM();
+		
+		try  {
+			em.getTransaction().begin();
+			result = em.find(user.class, id);
+			em.getTransaction().commit();
+			em.getTransaction().commit();
+		} catch (Exception e) {
+			throw new Exception("Hubo algún error", e);
+		}
+		
+		return result;
+	}
+	
+	public void updateUser(User u) throws Exception {
+		
+	}
+	
 }
---
+++ src/main/java/socketServidor/utils/PersistenceUnit.java
@@ -0,0 +1,57 @@
+package socketServidor.utils;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class PersistenceUnit {
+	private static EntityManagerFactory emf;
+	private static EntityManager em;
+	public static boolean mode = false; //false -> local & true -> remote
+	private static final String LOCAL = "aplicacionH2";
+	private static final String REMOTE = "aplicacionMariaDB";
+
+	public static EntityManagerFactory getInstance(String name) {
+		if (emf == null) {
+			if (name.equals(LOCAL) || name.equals(REMOTE)) {
+				emf = Persistence.createEntityManagerFactory(name);
+			}
+		}
+		return emf;
+	}
+
+//	public static EntityManagerFactory getRemoteInstance() {
+//		if (emf == null) {
+//			emf = Persistence.createEntityManagerFactory(REMOTE);
+//		}
+//		return emf;
+//	}
+//
+//	public static EntityManagerFactory getLocalInstance() {
+//		if (emf == null) {
+//			emf = Persistence.createEntityManagerFactory(LOCAL);
+//		}
+//		return emf;
+//	}
+	
+	public static EntityManagerFactory getInstance() {
+		if (emf == null) {
+			if(mode) {
+				emf = Persistence.createEntityManagerFactory(REMOTE);
+			}else {
+				emf = Persistence.createEntityManagerFactory(LOCAL);
+			}
+		}
+		return emf;
+	}
+	
+	public static EntityManager getEM() {
+		if(em==null) {
+			EntityManagerFactory emf = PersistenceUnit.getInstance();
+			em=emf.createEntityManager();
+			return em;			
+		}else {
+			return em;
+		}
+	}
+}
